b8ef2a52150e6747ac3a3ba926f5ab6dc1b45414
Dale Hamel <dale.hamel@shopify.com>
2015-06-17 13:29:59 -0400

    Support remote states

--- lib/cloudshaper/cli.rb
+++ lib/cloudshaper/cli.rb
@@ -3,7 +3,8 @@ require 'cloudshaper'
 
 module Cloudshaper
   class CLI < Thor
-    class_option "template_dir", "type" => "string", "default" => Dir.pwd
+    class_option "template_dir", type: "string", default: "templates"
+    class_option "remote_state", type: "boolean"
 
     desc "list", "List all available stacks"
     def list
@@ -14,51 +15,55 @@ module Cloudshaper
 
     desc "show NAME", "Show details about a stack by name"
     def show(name)
-      stack = load_stack(name)
+      stack = load_stack(name, options)
       puts stack
     end
 
     desc "plan NAME", "Show pending changes for a stack"
     def plan(name)
-      stack = load_stack(name)
+      stack = load_stack(name, options)
+      remote_config(name)
       stack.plan
     end
 
     desc "apply NAME", "Apply all pending stack changes"
     def apply(name)
-      stack = load_stack(name)
+      stack = load_stack(name, options)
       stack.apply
+      push(name) if remote_state?
     end
 
     desc "destroy NAME", "Destroy a stack"
     def destroy(name)
-      stack = load_stack(name)
+      stack = load_stack(name, options)
       stack.destroy
+      push(name) if remote_state?
     end
 
     desc "pull NAME", "Pull stack state from remote location"
     def pull(name)
-      stack = load_stack(name)
+      stack = load_stack(name, options, false)
       remote_config(name)
       stack.pull
     end
 
     desc "push NAME", "Push stack state from remote location"
     def push(name)
-      stack = load_stack(name)
+      stack = load_stack(name, options)
       remote_config(name)
       stack.push
     end
 
     desc "remote_config NAME", "Sets up remote config for a stack"
     def remote_config(name)
-      stack = load_stack(name)
+      stack = load_stack(name, options, false)
       stack.remote_config
     end
 
     desc "init", "Initialize stacks.yml if it does not exist"
     def init
       Cloudshaper::Stacks.init
+      puts "Created stacks.yml, you're ready to configure your stack"
     end
 
     desc "uuid", "Generate a UUID for your stacks, so they don't clobber each other"
@@ -70,13 +75,24 @@ module Cloudshaper
     def version
       puts Cloudshaper::VERSION
     end
+
   private
 
-    def load_stack(stack)
+    def load_stack(stack, options, pull=true)
+      load_modules(options['template_dir'])
       Cloudshaper::Stacks.load
-      stack = Cloudshaper::Stacks.stacks[args[:name]]
+      pull(stack) if remote_state? && pull
+      stack = Cloudshaper::Stacks.stacks[stack]
       stack.get
       stack
+    end
+
+    def remote_state?
+      options["remote_state"]
+    end
+
+    def load_modules(dir)
+      Dir.glob("#{File.join(Dir.pwd, dir)}/*.rb").each { |t| require_relative t }
     end
   end
 end
